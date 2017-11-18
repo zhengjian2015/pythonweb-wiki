1892dd20c6720f1f002d1df1234f35354039d22c
zj21zhengj <570470242@qq.com>
2017-11-18 21:02:15 +0800

    search function

--- cwiki.py
+++ cwiki.py
@@ -1,13 +1,14 @@
 #encoding:utf-8
-from config import * 
+
+from config import *
 
 urls = ("","Index","/", "Index",
-        "/rst/arts",    "ListArticle",
-        "/rst/img",     "Image",
-        "/rst/art",  "Article",
-	   "/.*",     "viewController")
-
-
+    "/rst/arts",        "ListArticle",
+    "/rst/art",         "Article",
+    "/rst/img",         "Image",
+    "/rst/att",         "Attachment",
+    "/rst/modiusers",   "SetModiUsers",
+    "/.*",              "viewController")
 module_urls = web.application(urls, locals())
 
 import mwiki
@@ -21,46 +22,49 @@ class viewController(baseViewController):
         baseViewController.GET(self)
         param = web.input()
         render = get_modulerender('wiki/views'+web.ctx.path)
-        web.debug(render)
         if render:
             ctxdata = get_ctx()
             pagedata = {}
-            web.debug(web.ctx.path)
-            web.debug("22222222222222222222222222222222")
             if web.ctx.path.startswith('/wklist'):
                 #views里面要有wklist,否则进不来
                 pagedata['datalist'],pagedata['datasize'] = mwiki.list_article(param)
                 #wklist页面里嵌套wkside页面
                 pageside = get_modulerender('wiki/views/wkside')
                 pagedata['sidepage'] = pageside
-                web.debug(pageside)
-                web.debug(param)
+                pagedata['keywords'] = mwiki.get_allkeywords()
+                pagedata['hotart'] = mwiki.list_hotarticle()
                 #前端好强大啊，引入blog.css前后完全不一样了。。。。。。。
                 return platLayout(render(ctxdata,param,pagedata),nobars=True,title='文章列表',
                                   addcss=['/static/css/pages/blog.css'])
             elif web.ctx.path.startswith('/wkv2'):
                 pageside = get_modulerender('wiki/views/wkside')
-                web.debug("??"*30)
                 pagedata['sidepage'] = pageside
+                pagedata['keywords'] = mwiki.get_allkeywords()
                 pagedata['artldata'] = mwiki.get_article(param.get('id',''))
                 if pagedata['artldata']:
-                    #pagedata['relateart'] = mwiki.list_relarticle(pagedata['artldata'])
-                    return platLayout(render(ctxdata,param,pagedata),nobars=True,title=pagedata['artldata'].get('ART_TITLE','查看文章'),
-                                     addcss=['/static/css/pages/blog.css','/x/wiki/static/css/style.css','/x/wiki/static/css/editormd.preview.min.css'],
-                                     addjs =['/x/wiki/static/lib/marked.min.js',
-                                              '/x/wiki/static/lib/prettify.min.js',
-                                              '/x/wiki/static/lib/raphael.min.js',
-                                              '/x/wiki/static/lib/underscore.min.js',
-                                              '/x/wiki/static/lib/sequence-diagram.min.js',
-                                              '/x/wiki/static/lib/flowchart.min.js',
-                                              '/x/wiki/static/lib/jquery.flowchart.min.js',
-                                              '/x/wiki/static/editormd.min.js'])
+                    pagedata['relateart'] = mwiki.list_relarticle(pagedata['artldata'])
+                    mwiki.increase_readcount(param.get('id',''))
+                return platLayout(render(ctxdata,param,pagedata),nobars=True,title=pagedata['artldata'].get('ART_TITLE','查看文章'),
+                                  addcss=['/static/css/pages/blog.css','/x/wiki/static/css/style.css','/x/wiki/static/css/editormd.preview.min.css'],
+                                  addjs =['/x/wiki/static/lib/marked.min.js',
+                                          '/x/wiki/static/lib/prettify.min.js',
+                                          '/x/wiki/static/lib/raphael.min.js',
+                                          '/x/wiki/static/lib/underscore.min.js',
+                                          '/x/wiki/static/lib/sequence-diagram.min.js',
+                                          '/x/wiki/static/lib/flowchart.min.js',
+                                          '/x/wiki/static/lib/jquery.flowchart.min.js',
+                                          '/x/wiki/static/editormd.min.js'])
             elif web.ctx.path.startswith('/wkedit'):
+                #编辑需要判断登录状态、操作员权限
+                baseViewController.GET(self)
                 artid = param.get('id','')
                 pagedata['article'] = mwiki.get_article(artid) if artid else {}
+                if pagedata['article']:
+                    if pagedata['article'].get('CREATE_USER','')!=ctxdata['usersession']['usercode'] and pagedata['article'].get('CANMODI_USERS','').find(ctxdata['usersession']['usercode'])<0:
+                        raise custom_error.internalerror('你没有权限编辑这篇文章：%s'%pagedata['article'].get('ART_TITLE',''))
             return render(ctxdata,param,pagedata)
-
-
+        else:
+            raise custom_error.notfound(web.ctx.path)
 
 class ListArticle(baseServiceController):
     def GET(self):
@@ -69,24 +73,46 @@ class ListArticle(baseServiceController):
         retval = mwiki.list_article(param)
         return formatting.json_string(retval[0])
 
-
-
 class Article(baseServiceController):
+    def GET(self):
+        '''取文章内容'''
+        param = web.input(id='')
+        if param.id:
+            retval = mwiki.get_article(param.id)
+        else:
+            retval = {}
+        return formatting.json_string(retval)
     def POST(self):
         '''保存文章内容'''
-        web.debug("xxx"*30)
         param = web.input()
-        web.debug(param)
         baseServiceController.POST(self,param)
         ctxdata = get_ctx()
         usercode = ctxdata['usersession']['usercode']
         retval = mwiki.set_article(param,usercode)
         return formatting.json_string(retval)
+    def DELETE(self):
+        '''删除文章'''
+        param = web.input()
+        baseServiceController.POST(self,param)
+        artid = param.get('id','')
+        article = mwiki.get_article(artid) if artid else {}
+        if article:
+            ctxdata = get_ctx()
+            if article.get('CREATE_USER','')!=ctxdata['usersession']['usercode']:
+                retval = {'ERROR':'你没有权限编辑这篇文章：%s'%article.get('ART_TITLE','')}
+            else:
+                artldata = {'ART_ID':artid,'STATUS':1}
+                retval = mwiki.set_article(artldata,ctxdata['usersession']['usercode'])
+                if not retval.has_key('ERROR'):
+                    retval = {'SUCCESS':'删除成功！'}
+        else:
+            retval = {'ERROR':'文章不存在！'}
+        return formatting.json_string(retval)
 
 class Image(baseServiceController):
+    """图片内容获取与上传"""
     def GET(self):
         param = web.input(id='')
-        web.debug(param)
         imgdata = mwiki.get_image(param.id) if param.id else None
         if imgdata:
             if param.get('t','')=='base64':
@@ -96,24 +122,75 @@ class Image(baseServiceController):
                 return base64.decodestring(imgdata)
         else:
             return ''
-
     def POST(self):
         '''用于给Editor.md上传图片用'''
         param = web.input(editormd_image_file={})
-        web.debug("**44"*30)
-        web.debug(param)    
         baseServiceController.POST(self,param)
         retmsg = {"success":1, "message":"上传成功", "url":"图片地址"}
-        if param.has_key("editormd_image_file"):
+        if param.has_key('editormd_image_file'):
             ctxdata = get_ctx()
             usercode = ctxdata['usersession']['usercode']
             retval = mwiki.set_image(param['editormd_image_file'].value,usercode,param['editormd_image_file'].filename)
-            if retval.has_key("ERROR"):
+            if retval.has_key('ERROR'):
                 retmsg['success'] = 0
                 retmsg['message'] = retval['ERROR']
             else:
                 retmsg['url'] = ctxdata.get('realhome','')+'/x/wiki/rst/img?id='+retval['IMG_ID']
         else:
             retmsg['success'] = 0
-            retmsg['message'] =  u"上传图标数据为空！"
+            retmsg['message'] = u"上传图标数据为空！"
         return formatting.json_string(retmsg)
+
+class Attachment(baseServiceController):
+    """附件内容获取与上传"""
+    def GET(self):
+        param = web.input(id='')
+        imgdata = mwiki.get_attach(param.id) if param.id else None
+        if imgdata:
+            if param.get('t','')=='base64':
+                return imgdata
+            else:
+                web.header('Content-Type', 'image/png')
+                return base64.decodestring(imgdata)
+        else:
+            return ''
+    def POST(self):
+        '''用于给Editor.md上传图片用'''
+        param = web.input(editormd_image_file={})
+        baseServiceController.POST(self,param)
+        retmsg = {"success":1, "message":"上传成功", "url":"图片地址"}
+        if param.has_key('editormd_image_file'):
+            ctxdata = get_ctx()
+            usercode = ctxdata['usersession']['usercode']
+            retval = mwiki.set_image(param['editormd_image_file'].value,usercode,param['editormd_image_file'].filename)
+            if retval.has_key('ERROR'):
+                retmsg['success'] = 0
+                retmsg['message'] = retval['ERROR']
+            else:
+                retmsg['url'] = ctxdata.get('realhome','')+'/x/wiki/rst/img?id='+retval['IMG_ID']
+        else:
+            retmsg['success'] = 0
+            retmsg['message'] = u"上传图标数据为空！"
+        return formatting.json_string(retmsg)
+
+class SetModiUsers(baseServiceController):
+    def GET(self):
+        '''取文章可修改用户'''
+        param = web.input(ART_ID='')
+        m = mwiki.get_article(param.ART_ID)
+        if not m:
+            return ''
+        elif m and m.has_key('ERROR'):
+            return  ''
+        elif m:
+            return m.CANMODI_USERS
+    def POST(self):
+        '''设置文章可修改用户'''
+        param = web.input(ART_ID='',canmodiusers='')
+        utils.debug(param)
+        baseServiceController.POST(self,param)
+        retval = mwiki.modi_users(param.ART_ID,param.canmodiusers)
+        if retval.has_key('ERROR'):
+            return retval['ERROR']
+        else:
+            return '设置成功'
--- mwiki.py
+++ mwiki.py
@@ -6,7 +6,6 @@ connWiki = getDataConnection('WIKI')
 connWiki = connWiki if connWiki else dbFrame
 
 ################################################################################
-
 def list_article(param):
     '''取文章列表'''
     if connWiki:
@@ -44,72 +43,170 @@ def list_article(param):
         return m[(pag_curr-1)*pag_size:(pag_curr)*pag_size],len(m)
     else:
         return []
-
 def get_article(artid):
-	'''获取文章'''
-	if connWiki:
-		m = web.listget(connWiki.select("WK_ARTICLES",vars=locals(),where="ART_ID=$artid").list(),0,None)
-		if m:
-			web.debug("88"*30)
-			web.debug(m)
-			return m
-		else:
-			return {"ERROR":'文章不存在!'}
-	else:
-		return {"ERROR":'No DB!'}
+    '''获取文章'''
+    if connWiki:
+        m = web.listget(connWiki.select("WK_ARTICLES",vars=locals(),where="ART_ID=$artid").list(),0,None)
+        if m:
+            usersession = get_userenv()
+            dictuser = usersession['userdict']
+            m["CREATE_NAME"] = dictuser.get(m['CREATE_USER'],m['CREATE_USER'])
+            m["UPDATE_NAME"] = dictuser.get(m['UPDATE_USER'],m['UPDATE_USER'])
+            m["CAN_MODI"] = (m['CREATE_USER']==usersession['usercode'] or (m['CANMODI_USERS'] and usersession['usercode'] and m['CANMODI_USERS'].find(usersession['usercode'])>=0))
+            m["CAN_DEL"] = (m['CREATE_USER']==usersession['usercode'])
+            m["CANMODI_NAMES"] = ','.join([usersession['userdict'].get(y,y) for y in m['CANMODI_USERS'].split(',')]) if m['CANMODI_USERS'] else ''
+            return m
+        else:
+            return {"ERROR":'文章不存在!'}
+    else:
+        return {"ERROR":'No DB!'}
 def set_article(param,usercode):
-	'''报错文章'''
-	if connWiki:
-		artid = param.pop('ART_ID','')
-		t = connWiki.transaction()
-		try:
-			if artid:
-				param['UPDATE_USER'] = usercode
-				param['UPDATE_TIME'] = utils.get_currdatetimestr(connWiki.dbtype)
-				connWiki.update("WK_ARTICLES",vars=locals(),where="ART_ID=$artid",**param)
-			else:
-				artid = utils.get_keyword()
-				param['CREATE_USER'] = usercode
-				param['CREATE_TIME'] = utils.get_currdatetimestr(connWiki.dbtype)
-				param['UPDATE_USER'] = usercode
-				param['UPDATE_TIME'] = utils.get_currdatetimestr(connWiki.dbtype)
-				connWiki.insert("WK_ARTICLES",ART_ID=artid,**param)
-		except Exception, e:
-			t.rollback()
-			traceback.print_exc()
-			return {"ERROR":e}
-		else:
-			t.commit()
-			return {'ART_ID':artid,'MSG':'保存成功！'}
-	else:
-		return {"ERROR":'No DB!'}
+    '''保存文章'''
+    if connWiki:
+        artid = param.pop('ART_ID','')
+        t = connWiki.transaction()
+        try:
+            if artid:
+                param['UPDATE_USER'] = usercode
+                param['UPDATE_TIME'] = utils.get_currdatetimestr(connWiki.dbtype)
+                connWiki.update("WK_ARTICLES",vars=locals(),where="ART_ID=$artid",**param)
+            else:
+                artid = utils.get_keyword()
+                param['CREATE_USER'] = usercode
+                param['CREATE_TIME'] = utils.get_currdatetimestr(connWiki.dbtype)
+                param['UPDATE_USER'] = usercode
+                param['UPDATE_TIME'] = utils.get_currdatetimestr(connWiki.dbtype)
+                connWiki.insert("WK_ARTICLES",ART_ID=artid,**param)
+        except Exception, e:
+            t.rollback()
+            traceback.print_exc()
+            return {"ERROR":e}
+        else:
+            t.commit()
+            return {'ART_ID':artid,'MSG':'保存成功！'}
+    else:
+        return {"ERROR":'No DB!'}
+def modi_users(artid,users):
+    '''设置文章可修改用户'''
+    if not connWiki:
+        return {"ERROR":'No DB!'}
+    else:
+        t = connWiki.transaction()
+        try:
+            connWiki.update("WK_ARTICLES",vars=locals(),where="ART_ID=$artid",CANMODI_USERS=users)
+        except Exception, e:
+            t.rollback()
+            traceback.print_exc()
+            return {"ERROR":e}
+        else:
+            t.commit()
+            return {'ART_ID':artid,'MSG':'保存成功！'}
 
+def increase_readcount(artid):
+    '''指定文章的次数加1'''
+    if connWiki:
+        t = connWiki.transaction()
+        try:
+            connWiki.update("WK_ARTICLES",vars=locals(),where="ART_ID=$artid",MATCH_TIMES=web.SQLLiteral('MATCH_TIMES+1'))
+        except Exception, e:
+            t.rollback()
+            traceback.print_exc()
+            return {"ERROR":e}
+        else:
+            t.commit()
+            return {'ART_ID':artid,'MSG':'保存成功！'}
+    else:
+        return {"ERROR":'No DB!'}
+def get_allkeywords():
+    '''得到所有未删除文章的关键词，以{'word':count}形式返回'''
+    if connWiki:
+        m = connWiki.select("WK_ARTICLES",what="ART_KEYWORDS",where="STATUS=0").list()
+        tmpl = []
+        for x in m:
+            tmpl += x.ART_KEYWORDS.split(' ')
+        words = {}
+        for x in tmpl:
+            if x:
+                words[x] = words.get(x,0)+1
+        words = sorted(words.iteritems(), key=lambda d:d[1], reverse = True)
+        return words
+    else:
+        return {"ERROR":'No DB!'}
+def list_hotarticle(limit=5):
+    '''返回阅读次数最多的n篇文章'''
+    if connWiki:
+        usersession = get_userenv()
+        dictuser = usersession['userdict']
+        m = connWiki.select("WK_ARTICLES",vars=locals(),where='STATUS=0',order="MATCH_TIMES DESC",limit=limit).list()
+        m = [dict(x, **{ "CREATE_NAME":dictuser.get(x['CREATE_USER'],x['CREATE_USER']), "UPDATE_NAME":dictuser.get(x['UPDATE_USER'],x['UPDATE_USER']), "CAN_MODI":x['CREATE_USER']==usersession['usercode'] }) for x in m]
+        return m
+    else:
+        return []
+def list_relarticle(article,limit=10):
+    '''返回指定文章的相关n篇文章'''
+    if connWiki:
+        usersession = get_userenv()
+        dictuser = usersession['userdict']
+        clause_where = ["STATUS=0 AND ART_ID<>'%s'"%article.ART_ID]
+        clause_where.append(utils.get_where_clause2(article.ART_KEYWORDS,['ART_KEYWORDS']))
+        clause_where = ' and '.join(clause_where) if clause_where else None
+        m = connWiki.select("WK_ARTICLES",vars=locals(),where=clause_where,order="MATCH_TIMES DESC",limit=limit).list()
+        m = [dict(x, **{ "CREATE_NAME":dictuser.get(x['CREATE_USER'],x['CREATE_USER']), "UPDATE_NAME":dictuser.get(x['UPDATE_USER'],x['UPDATE_USER']), "CAN_MODI":x['CREATE_USER']==usersession['usercode'] }) for x in m]
+        return m
+    else:
+        return []
+
+################################################################################
 def get_image(imgid):
-	'''获取图片'''
-	if connWiki:
-		m = web.listget(connWiki.select("WK_IMAGES",vars=locals(),where="IMG_ID=$imgid").list(),0,None)
-		return m.FILEDATA if m else None
-	else:
-		return None
-
+    """获取图片"""
+    if connWiki:
+        m = web.listget(connWiki.select("WK_IMAGES",vars=locals(),where="IMG_ID=$imgid").list(),0,None)
+        return m.FILEDATA if m else None
+    else:
+        return None
 def set_image(imgdata,usercode,filename=''):
-	'''上传图片'''
-	if connWiki:
-		#摘要算法简介 imgid类似uuid 是唯一的
-		imgid = hashlib.sha1(imgdata).hexdigest()
-		web.debug(imgid)
-		m = web.listget(connWiki.select("WK_IMAGES",vars=locals(),where="IMG_ID=$imgid").list(),0,None)
-		web.debug(m)
-		if not m:
-			t = connWiki.transaction()
-			try:
-				connWiki.insert("WK_IMAGES",IMG_ID=imgid,FILEDATA=base64.encodestring(imgdata),FILESIZE=len(imgdata),FILENAME=filename,CREATE_USER=usercode,CREATE_TIME=utils.get_currdatetimestr(connWiki.dbtype))
-			except Exception, e:
-				t.rollback()
-				traceback.print_exc()
-				return {"ERROR":e}
-			else:
-				t.commit()
-		return {"IMG_ID":imgid}
-	else:
-		return {"ERROR":'No DB!'}
+    """上传图片"""
+    if connWiki:
+        imgid = hashlib.sha1(imgdata).hexdigest()
+        m = web.listget(connWiki.select("WK_IMAGES",vars=locals(),where="IMG_ID=$imgid").list(),0,None)
+        if not m:
+            t = connWiki.transaction()
+            try:
+                connWiki.insert("WK_IMAGES",IMG_ID=imgid,FILEDATA=base64.encodestring(imgdata),FILESIZE=len(imgdata),FILENAME=filename,CREATE_USER=usercode,CREATE_TIME=utils.get_currdatetimestr(connWiki.dbtype))
+            except Exception, e:
+                t.rollback()
+                traceback.print_exc()
+                return {"ERROR":e}
+            else:
+                t.commit()
+        return {"IMG_ID":imgid}
+    else:
+        return {"ERROR":'No DB!'}
+
+################################################################################
+def get_attach(attid):
+    """获取附件"""
+    if connWiki:
+        m = web.listget(connWiki.select("WK_ATTACHS",vars=locals(),where="ATT_ID=$attid").list(),0,None)
+        return m.FILEDATA if m else None
+    else:
+        return None
+def set_attach(filedata,usercode,filename=''):
+    '''保存附件'''
+    if connWiki:
+        attid = hashlib.sha1(imgdata).hexdigest()
+        m = web.listget(connWiki.select("WK_ATTACHS",vars=locals(),where="ATT_ID=$attid").list(),0,None)
+        if not m:
+            t = connWiki.transaction()
+            try:
+                connWiki.insert("WK_ATTACHS",ATT_ID=attid,FILEDATA=base64.encodestring(filedata),FILESIZE=len(filedata),FILENAME=filename,CREATE_USER=usercode,CREATE_TIME=utils.get_currdatetimestr(connWiki.dbtype))
+            except Exception, e:
+                t.rollback()
+                traceback.print_exc()
+                return {"ERROR":e}
+            else:
+                t.commit()
+        return {"ATT_ID":attid}
+    else:
+        return {"ERROR":'No DB!'}
+
